6ec82fae48e8978f8a85dd2d3eaa676ea1c5aa9f
Leonardo <leonardo.bongianni@gmail.com>
2021-04-28 16:48:26 +0200

    Esercizio Completo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Creare una variabile con un paragrafo di testo. 
 Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con ***.
+
+![](screen.png);
--- index.php
+++ index.php
@@ -4,41 +4,45 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">  
+  <link rel="stylesheet" href="master.css">
   <title>BAD WORDS NONO</title>
 </head>
-<body>
+<body class="flex">
   <div class="paragraph">
     <p>
       <span>Chiamata Get</span><br>
       <?php
         $testo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ante metus, vehicula sed luctus id, imperdiet nec erat. Nam at fringilla nibh. Maecenas tincidunt metus ornare lacus faucibus, a lacinia ante hendrerit. Etiam eget imperdiet diam, sit amet sagittis massa. Pellentesque quis porttitor nisl, ullamcorper facilisis diam. Etiam velit sapien, condimentum ac augue id, blandit sollicitudin nunc. Vestibulum non tellus eget est aliquam congue sed nec justo. In vitae consectetur lectus, consectetur volutpat ex. Donec tortor nulla, malesuada in ipsum sit amet, ultricies varius velit.";
-        $testo = str_replace($_GET["badword"], "***", $testo);
-        echo $testo;
+        $badwordget = $_GET["badword"];
+        $newtext = str_replace($badwordget, "***", $testo);
+        echo $newtext;
       ?>
     </p>
     <p>
       <?php
-        $length = strlen($testo);
+        $length = strlen($newtext);
         echo "Lunghezza paragrafo:" . " " . $length;
       ?>
     </p>
   </div>
   <div class="paragraph-two">
     <p>
-      <span>Sostituita la parola "ipsum"<span><br>
+      <span>Sostituita la parola "ipsum"</span><br>
       <?php
         $testo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ante metus, vehicula sed luctus id, imperdiet nec erat. Nam at fringilla nibh. Maecenas tincidunt metus ornare lacus faucibus, a lacinia ante hendrerit. Etiam eget imperdiet diam, sit amet sagittis massa. Pellentesque quis porttitor nisl, ullamcorper facilisis diam. Etiam velit sapien, condimentum ac augue id, blandit sollicitudin nunc. Vestibulum non tellus eget est aliquam congue sed nec justo. In vitae consectetur lectus, consectetur volutpat ex. Donec tortor nulla, malesuada in ipsum sit amet, ultricies varius velit.";
-        $testo = str_replace("ipsum", "***", $testo);
-        echo $testo;
+        $badword = "ipsum";
+        $textcensored = str_replace($badword, "***", $testo);
+        echo $textcensored;
       ?>
     </p>
     <p>
       <?php
-        $length = strlen($testo);
+        $length = strlen($textcensored);
         echo "Lunghezza paragrafo:" . " " . $length;
       ?>
     </p>
   </div>
-  
 </body>
 </html>
